88b60179833deb7052027307453239b76d7631a7
Raphael Bastos <5449417+coffnix@users.noreply.github.com>
2024-07-25 01:10:11 -0300

    Update scripts to check for Xcode or Xcode-beta, fix macOS 15.0 Sequoia bug

--- src/lib/platform/OSXDragSimulator.mm
+++ src/lib/platform/OSXDragSimulator.mm
@@ -29,74 +29,78 @@ NSString* g_ext = NULL;
 void
 runCocoaApp()
 {
-	NSAutoreleasePool* pool = [[NSAutoreleasePool alloc] init];
+    NSAutoreleasePool* pool = [[NSAutoreleasePool alloc] init];
 
-	[NSApplication sharedApplication];
+    [NSApplication sharedApplication];
 
     NSWindow* window = [[NSWindow alloc]
-						initWithContentRect: NSMakeRect(0, 0, 3, 3)
-						styleMask: NSBorderlessWindowMask
-						backing: NSBackingStoreBuffered
-						defer: NO];
-    [window setTitle: @""];
-	[window setAlphaValue:0.1];
-	[window makeKeyAndOrderFront:nil];
+                        initWithContentRect:NSMakeRect(0, 0, 3, 3)
+                        styleMask:(NSTitledWindowMask | NSWindowStyleMaskTitled)
+                        backing:NSBackingStoreBuffered
+                        defer:NO];
+    [window setTitle:@""];
+    [window setAlphaValue:0.1];
 
-	OSXDragView* dragView = [[OSXDragView alloc] initWithFrame:NSMakeRect(0, 0, 3, 3)];
+    // Certifique-se de que isso seja executado na thread principal
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [window makeKeyAndOrderFront:nil];
+    });
 
-	g_dragWindow = window;
-	g_dragView = dragView;
-	[window setContentView: dragView];
+    OSXDragView* dragView = [[OSXDragView alloc] initWithFrame:NSMakeRect(0, 0, 3, 3)];
 
-	NSLog(@"starting cocoa loop");
-	[NSApp run];
+    g_dragWindow = window;
+    g_dragView = dragView;
+    [window setContentView:dragView];
 
-	NSLog(@"cocoa: release");
-	[pool release];
+    NSLog(@"starting cocoa loop");
+    [NSApp run];
+
+    NSLog(@"cocoa: release");
+    [pool release];
 }
 
 void
 stopCocoaLoop()
 {
-	[NSApp stop: g_dragWindow];
+    [NSApp stop:g_dragWindow];
 }
 
 void
 fakeDragging(const char* str, int cursorX, int cursorY)
 {
-	g_ext = [NSString stringWithUTF8String:str];
+    g_ext = [NSString stringWithUTF8String:str];
 
-	dispatch_async(dispatch_get_main_queue(), ^{
-	NSRect screen = [[NSScreen mainScreen] frame];
-	NSLog ( @"screen size: width = %f height = %f", screen.size.width, screen.size.height);
-	NSLog ( @"mouseLocation: %d %d", cursorX, cursorY);
+    dispatch_async(dispatch_get_main_queue(), ^{
+        NSRect screen = [[NSScreen mainScreen] frame];
+        NSLog(@"screen size: width = %f height = %f", screen.size.width, screen.size.height);
+        NSLog(@"mouseLocation: %d %d", cursorX, cursorY);
 
-	int newPosX = 0;
-	int newPosY = 0;
-	newPosX = cursorX - 1;
-	newPosY = screen.size.height - cursorY - 1;
+        int newPosX = 0;
+        int newPosY = 0;
+        newPosX = cursorX - 1;
+        newPosY = screen.size.height - cursorY - 1;
 
-	NSRect rect = NSMakeRect(newPosX, newPosY, 3, 3);
-	NSLog ( @"newPosX: %d", newPosX);
-	NSLog ( @"newPosY: %d", newPosY);
+        NSRect rect = NSMakeRect(newPosX, newPosY, 3, 3);
+        NSLog(@"newPosX: %d", newPosX);
+        NSLog(@"newPosY: %d", newPosY);
 
-	[g_dragWindow setFrame:rect display:NO];
-	[g_dragWindow makeKeyAndOrderFront:nil];
-	[NSApp activateIgnoringOtherApps:YES];
+        [g_dragWindow setFrame:rect display:NO];
+        [g_dragWindow makeKeyAndOrderFront:nil];
+        [NSApp activateIgnoringOtherApps:YES];
 
-	[g_dragView setFileExt:g_ext];
+        [g_dragView setFileExt:g_ext];
 
-	CGEventRef down = CGEventCreateMouseEvent(CGEventSourceCreate(kCGEventSourceStateHIDSystemState), kCGEventLeftMouseDown, CGPointMake(cursorX, cursorY), kCGMouseButtonLeft);
-	CGEventPost(kCGHIDEventTap, down);
-	});
+        CGEventRef down = CGEventCreateMouseEvent(CGEventSourceCreate(kCGEventSourceStateHIDSystemState), kCGEventLeftMouseDown, CGPointMake(cursorX, cursorY), kCGMouseButtonLeft);
+        CGEventPost(kCGHIDEventTap, down);
+    });
 }
 
 CFStringRef
 getCocoaDropTarget()
 {
-	// HACK: sleep, wait for cocoa drop target updated first
-	usleep(1000000);
-	return [g_dragView getDropTarget];
+    // HACK: sleep, wait for cocoa drop target updated first
+    usleep(1000000);
+    return [g_dragView getDropTarget];
 }
 
 #endif
